b5d7e6585cf89c97a4da640ec33b35420b4d3233
Norasmadi AR <norasmadi@gmail.com>
2022-12-03 20:00:04 +0800

    Active LOW Push Button at pin GP20

---
+++ 03 Push Button (GP20sw_blinkled_GP20.py
@@ -0,0 +1,22 @@
+"""Example for Pico. Turns on the An External LED (GP0)"""
+"""Push the Button at pin GP20 (Active LOW)"""
+
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.GP0)
+led.direction = digitalio.Direction.OUTPUT
+
+switch = digitalio.DigitalInOut(board.GP20)
+switch.direction = digitalio.Direction.INPUT
+
+while True:
+    if switch.value:
+        led.value = False
+    else:
+        print(switch.value)
+        led.value = True
+        time.sleep(0.5)
+        led.value = False
+        time.sleep(0.5)
