d3c0f7e865c8d0ee2d2cc042ea667fd5416ce62d
Norasmadi AR <norasmadi@gmail.com>
2023-01-06 12:41:33 +0800

    Pi Pico Serial Communication using readline()

---
+++ Serial Communication with PyCharm/Example2/code.py
@@ -0,0 +1,18 @@
+"""
+CircuitPython Essentials UART Serial example using readline
+"""
+
+import board
+import busio
+import time
+
+# UART
+uart = busio.UART(board.GP4, board.GP5, timeout=0.1,baudrate=9600)
+
+
+while True:
+    # Read byte from Serial    
+    # Read a line, ending in a newline character
+    byte_read = uart.readline()     
+    if byte_read is not None:
+        print(byte_read)
