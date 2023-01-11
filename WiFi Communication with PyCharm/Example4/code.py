b935ae03f9043905519e300d1f92da5414e354c3
Norasmadi AR <norasmadi@gmail.com>
2023-01-11 08:08:54 +0800

    Pi Pico W as Server

---
+++ WiFi Communication with PyCharm/Example4/code.py
@@ -0,0 +1,42 @@
+'''
+Pi Pico W: UDP Server communicate with UDP Client (PyCharm)
+'''
+import wifi
+import socketpool
+import ipaddress
+import time
+import os
+
+HOST = ""  # see below
+PORT = 5000
+TIMEOUT = None
+BACKLOG = 2
+MAXBUF = 256
+
+# Get wifi details from a settings.toml file
+print(os.getenv("test_env_file"))
+ssid = os.getenv("WIFI_SSID")
+password = os.getenv("WIFI_PASSWORD")
+print("Connecting to WiFi '{}' ... ".format(ssid), end="")
+pool = socketpool.SocketPool(wifi.radio)
+
+
+print("Self IP", wifi.radio.ipv4_address)
+HOST = str(wifi.radio.ipv4_address)
+server_ipv4 = ipaddress.ip_address(pool.getaddrinfo(HOST, PORT)[0][4][0])
+print("Server ping", server_ipv4, wifi.radio.ping(server_ipv4), "ms")
+
+print("Create TCP Server socket", (HOST, PORT))
+s = pool.socket(pool.AF_INET, pool.SOCK_DGRAM)
+s.settimeout(TIMEOUT)
+
+s.bind((HOST, PORT))
+
+buf = bytearray(MAXBUF)
+
+while True:
+    size, addr = s.recvfrom_into(buf)
+    print("Received", buf[:size], size, "bytes from", addr)
+
+    size = s.sendto(buf[:size], addr)
+    print("Sent", buf[:size], size, "bytes to", addr)
